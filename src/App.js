5fefce6c7dd69e2d8c773b90e47be8e907b058a9
gamesatem <games.atem@gmail.com>
2021-09-21 17:20:46 +0300

    Basic zip image viewing and styling

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef } from "react";
+import JSZip from "jszip";
+import { Button } from "semantic-ui-react";
+
+import "./App.css";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const images = useRef();
+    const selectButton = useRef();
+    const fileInput = useRef();
+
+    const handleSelectButton = () => {
+        fileInput.current.click();
+    };
+
+    const handleFile = async (e) => {
+        const selectedFile = e.target.files[0];
+        if (selectedFile.type !== "application/x-zip-compressed") return;
+        images.current.innerHTML = "";
+        const zip = await JSZip.loadAsync(selectedFile);
+        for (const name in zip.files) {
+            if (Object.hasOwnProperty.call(zip.files, name)) {
+                const blob = await zip.file(name).async("blob");
+                const image = new Image();
+                image.src = URL.createObjectURL(blob);
+                images.current.appendChild(image);
+            }
+        }
+    };
+
+    return (
+        <div className="App">
+            <input ref={fileInput} type="file" hidden onChange={handleFile} />
+            <Button
+                ref={selectButton}
+                onClick={handleSelectButton}
+                primary
+                content="Select .zip File"
+            />
+            <div className="ImageViewer" ref={images}></div>
+        </div>
+    );
 }
 
 export default App;
